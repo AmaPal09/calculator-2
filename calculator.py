c80a1f4a3acd0fac04bd837c7cd68f76c5be3947
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-19 16:14:19 -0800

    Inital commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+loop_bool = True
+
+while loop_bool:
+    input_str = input(">")
+    loop_bool = False
+
+# Your code goes here
