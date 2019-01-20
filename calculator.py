8e72787b884507b7d3fc465e28baa712598bf3a2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-19 16:26:41 -0800

    Handled addition

--- calculator.py
+++ calculator.py
@@ -4,13 +4,26 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
+import arithmetic
 
 
 loop_bool = True
 
 while loop_bool:
-    input_str = input(">")
-    loop_bool = False
+    input_str = input("> ")
+    input_list = input_str.split(" ")
+
+    if input_list[0] == 'q': 
+        loop_bool = False 
+        quit()
+    else:
+        solution = 0
+        if input_list[0] == '+':
+            solution = arithmetic.add(float(input_list[1]), float(input_list[2]))
+        print(solution)
+
+        
+
+    
 
 # Your code goes here
